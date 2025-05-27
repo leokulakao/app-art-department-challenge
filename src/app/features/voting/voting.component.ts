ef30169619b806f364e15f4f3a17024a0e90b68b
Kamil Mendelski <kamil@frontlabs.pl>
2025-05-27 11:03:34 +0200

    feat(voting): show icons with voting status

--- src/app/features/voting/voting.component.ts
+++ src/app/features/voting/voting.component.ts
@@ -20,7 +20,11 @@ import { VotingArticlesStore } from '../../common/signal-store/voting-articles.s
               votingArticle of votingArticlesStore.votingArticlesWithPhoto();
               track $index
             ) {
-              <div class="article__item">
+              <div
+                class="article__item"
+                [class]="{
+                  'article__item--voted': showVotedButton(votingArticle),
+                }">
                 <a
                   class="article__link"
                   [routerLink]="'item/' + votingArticle.id">
@@ -29,6 +33,16 @@ import { VotingArticlesStore } from '../../common/signal-store/voting-articles.s
                     [src]="votingArticle.mainPhotoUrl"
                     alt="Image" />
                   <span class="article__title">{{ votingArticle.title }}</span>
+
+                  <svg
+                    class="article__done"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24"
+                    viewBox="0 -960 960 960"
+                    width="24">
+                    <path
+                      d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q65 0 123 19t107 53l-58 59q-38-24-81-37.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160q133 0 226.5-93.5T800-480q0-18-2-36t-6-35l65-65q11 32 17 66t6 70q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-56-216L254-466l56-56 114 114 400-401 56 56-456 457Z" />
+                  </svg>
                 </a>
               </div>
             }
@@ -54,5 +68,17 @@ export class VotingComponent {
   protected onSend() {
     this.authService.logout();
     this.router.navigate(['/']);
+  }
+
+  protected showVotedButton({ candidatePhotoViews }: any) {
+    if (Array.isArray(candidatePhotoViews)) {
+      return candidatePhotoViews.find(
+        votingArticle => votingArticle.isVoted === true
+      )
+        ? true
+        : false;
+    }
+
+    return false;
   }
 }
